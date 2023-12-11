4220949c162e21a1988145d4040269013770c592
rshashank11 <shashankyadav2000@gmail.com>
2023-12-11 19:58:30 +0530

    Correction commit

--- src/api/baseAPI.js
+++ src/api/baseAPI.js
@@ -14,6 +14,7 @@ export const baseAPI = axios.create({
   baseURL: baseURL,
   headers: {
     accept: "application/json",
-    Authorization: `${apiToken}`,
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZDFmZTRkN2FhNWI4NTUwNTg1ZjYzMzFkYzQzZTQzOSIsInN1YiI6IjY1MzNkYTM1MmIyMTA4MDBmZjg0MmFlYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.IJeZV1prWYi-gv7diJui2qYqudRIi4eYGkOQbSPMn1E",
   },
 })
