d0a62b644b4ea24ab8ccea20376b30e5312e1a7f
rshashank11 <shashankyadav2000@gmail.com>
2023-12-11 19:48:22 +0530

    Error management commit

--- src/api/baseAPI.js
+++ src/api/baseAPI.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-import OpenAI from "openai"
+// import OpenAI from "openai"
 export const baseURL = "https://api.themoviedb.org/3"
 const apiToken = import.meta.env.VITE_BEARER_TOKEN
 // const apiKey = import.meta.env.VITE_OPEN_API_KEY
