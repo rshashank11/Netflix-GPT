16c8473674e585ba9594ea54af537964560bfd83
rshashank11 <shashankyadav2000@gmail.com>
2023-12-11 13:39:59 +0530

    Added modal feature

--- src/components/MovieOrTVList.jsx
+++ src/components/MovieOrTVList.jsx
@@ -6,8 +6,10 @@ import "../../node_modules/swiper/swiper-bundle.min.css"
 import { Navigation, A11y } from "swiper/modules"
 import "../App.css"
 import "../index.css"
+import ModalReact from "./DetailsModal"
 
 const MovieOrTVList = ({ mediaType, title, listData }) => {
+  // console.log(listData)
   return (
     <div>
       <h2 className="pl-7 text-xl font-bold leading-10 text-netflixRed tracking-tighter">
@@ -25,24 +27,24 @@ const MovieOrTVList = ({ mediaType, title, listData }) => {
           slidesPerGroup={4}
           speed={500}
           navigation
-          onSlideChange={() => console.log("slide change")}
-          onSwiper={(swiper) => console.log(swiper)}
         >
           {Array.isArray(listData)
-            ? listData.map((item) => {
+            ? listData.map((item, index) => {
+                // console.log(item)
                 return (
-                  <SwiperSlide>
+                  <SwiperSlide key={index}>
                     <li className="min-w-[185px] " key={item.id}>
-                      <Link
+                      {/* <Link
                         to={`/${mediaType ? mediaType : item.media_type}/${
                           item.id
                         }`}
-                      >
-                        <img
-                          className="min-w-[185px] h-[270px]"
-                          src={`https://image.tmdb.org/t/p/original/${item.poster_path}`}
-                        />
-                      </Link>
+                      > */}
+
+                      {/* </Link> */}
+                      <ModalReact
+                        {...item}
+                        mediaType={mediaType ? mediaType : item.media_type}
+                      />
                     </li>
                   </SwiperSlide>
                 )
